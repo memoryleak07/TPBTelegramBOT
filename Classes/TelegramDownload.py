12111f6f2df2f8d2aa26a683008e98b47cd9f5df
Pietro <pietroromano94@gmail.com>
2023-04-06 14:40:34 +0200

    fix for show download list fix for select path

--- Classes/TelegramDownload.py
+++ Classes/TelegramDownload.py
@@ -32,6 +32,7 @@ dataManage = DataManage()
 destinationPathKey = 'destinationPath'
 chatIdKey = 'chat_id'
 jobStatusKey = 'jobStatus'
+last_dir_message = 'message_dir_id'
 SPECIFY, DOC = range(2)
 
 
@@ -69,30 +70,37 @@ async def space_on_specify(update: Update, context: CallbackContext):
 
 async def dir_specify(update: Update, context: CallbackContext):
     """Reply dir to save"""
-    message = '\n/next to download or'
-    if (update.callback_query):
+    if update.callback_query:
         await set_path(context, update.callback_query.data)
-        await update.callback_query.edit_message_reply_markup(None)
-        await update.callback_query.message.reply_text(f'{await get_path(context)}{message}')
+        # controllo per cancellare l'ultimo messaggio
+        if last_dir_message in context.user_data:
+            await context.bot.deleteMessage(message_id=context.user_data[last_dir_message],
+                                      chat_id=update.callback_query.message.chat_id)
     else:
+        # controllo per cancellare l'ultimo messaggio
+        if last_dir_message in context.user_data:
+            await context.bot.deleteMessage(message_id=context.user_data[last_dir_message],
+                                            chat_id=update.message.chat_id)
         await set_path(context, update.message.text)
-        await update.message.reply_text(f'{await get_path(context)}{message}')
-    await ls_command(update, context)
+
+    message = f'{await get_path(context)}\n/next to download or\n'
+    await ls_command(update, context, message)
     return SPECIFY
 
 
-async def ls_command(update: Update, context: CallbackContext):
+async def ls_command(update: Update, context: CallbackContext, message=""):
     """Show list of directory"""
-    message = 'select or send the next directory name (for create or move)'
+    message = f'{message}select or send the next directory name (for create or move)'
     directories = PathManage.GetInlineAllDirectories(await get_path(context))
-    if (update.callback_query):
-        await update.callback_query.message.reply_text(message, reply_markup=InlineKeyboardMarkup(
+    if update.callback_query:
+        dir_message = await update.callback_query.message.reply_text(message, reply_markup=InlineKeyboardMarkup(
                                     inline_keyboard=directories, resize_keyboard=True)
                                     )
     else:
-        await update.message.reply_text(message, reply_markup=InlineKeyboardMarkup(
+        dir_message = await update.message.reply_text(message, reply_markup=InlineKeyboardMarkup(
                                     inline_keyboard=directories, resize_keyboard=True)
                                     )
+    context.user_data[last_dir_message] = dir_message.message_id
 
 
 async def ls_dir_command(update: Update, context: CallbackContext):
@@ -210,8 +218,19 @@ async def start_job(update: Update, context: CallbackContext):
 
 async def dw_list(update: Update, context: CallbackContext):
     """Send list of download and status"""
-    await update.message.reply_text("\n".join(await dataManage.get_view_download_list()))
-    await update.message.reply_text("Send /setsNew for set all download status \"ERROR\" in \"NEW\"")
+    download_list = await dataManage.get_view_download_list()
+    items_length = 20
+    if len(download_list) > 0:
+        for items in zip(*(iter(download_list),) * items_length):
+            await update.message.reply_text("\n".join(items))
+        # controllo per stampa degli ultimi items
+        item_module = len(download_list) % items_length
+        if item_module != 0:
+            await update.message.reply_text("\n".join(download_list[-item_module:]))
+        await update.message.reply_text(f"{len(download_list)} items in download list")
+        await update.message.reply_text("Send /setsNew for set all download status \"ERROR\" in \"NEW\"")
+    else:
+        await update.message.reply_text("Download list is empty")
 
     return DOC
 
